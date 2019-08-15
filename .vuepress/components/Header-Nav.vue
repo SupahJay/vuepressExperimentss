4c6da99e96ed9c9987392f19c62c73c3704c4750
SupahJay <moabelimagomes@hotmail.com>
2019-08-15 18:29:05 -0300

    nav href link fix

--- .vuepress/components/Header-Nav.vue
+++ .vuepress/components/Header-Nav.vue
@@ -1,6 +1,7 @@
 <template>
   <header class="main-nav">
-      <a v-for="(i, index) in frontpage($site.pages)" :href="'..'+i.path">{{i.title}}</a>
+      <!-- run a loop on items from the result of frontpage() -->
+      <a v-for="(i, index) in frontpage($site.pages)" :href="(/*$page.frontmatter.home?'':*/$site.base)+i.path.replace('/','')">{{i.title}}</a>
   </header>
 </template>
 <script>
